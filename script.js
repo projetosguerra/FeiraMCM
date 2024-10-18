9357f6da46d4c0b00653e21c1a7872005d7389ba
Pietro Guerra <100379604+projetosguerra@users.noreply.github.com>
2024-10-18 17:49:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function prosseguir() {
+    window.location.href = "indentificar.html";
+}
+
+function escolherAluno() {
+    window.location.href = "loginAluno.html";
+}
+
+function escolherProf() {
+    window.location.href = "loginProfessor.html";
+}
+
+function token() {
+    window.location.href = "token.html";
+}
